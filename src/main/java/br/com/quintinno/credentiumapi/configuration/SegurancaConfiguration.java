a45a1d7b543dfc2cbf684ddefe39fa8fe048d5ed
José Quintinno <josequintino@hotmail.com.br>
2025-06-27 16:21:21 -0300

    feature: Implementar endpoint de criptografia e descriptografia

---
+++ src/main/java/br/com/quintinno/credentiumapi/configuration/SegurancaConfiguration.java
@@ -0,0 +1,26 @@
+package br.com.quintinno.credentiumapi.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SegurancaConfiguration {
+	
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+		httpSecurity.csrf(csrf -> csrf.disable())
+				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+				.authorizeHttpRequests(authorize -> authorize.requestMatchers(HttpMethod.POST, "/credencium/criptografia/encoder/aes").permitAll()
+						.anyRequest().authenticated())
+				.cors(Customizer.withDefaults());
+		return httpSecurity.build();
+	}
+
+}
---
+++ src/main/java/br/com/quintinno/credentiumapi/controller/CriptografiaController.java
@@ -0,0 +1,35 @@
+package br.com.quintinno.credentiumapi.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.quintinno.credentiumapi.service.CriptografiaService;
+
+@RestController
+@RequestMapping("/credencium/criptografia")
+public class CriptografiaController {
+	
+	@Autowired
+	private CriptografiaService criptografiaService;
+	
+	@PostMapping("/encoder/aes")
+	public ResponseEntity<String> criptografarAES(@RequestBody String senha) {
+		return ResponseEntity.ok().body(this.criptografiaService.criptografar(senha));
+	}
+	
+	@PostMapping("/decoder/aes")
+	public ResponseEntity<String> descriptografarAES(String senha) {
+		return ResponseEntity.ok().body(this.criptografiaService.descriptografar(senha));
+	}
+	
+	@GetMapping
+	public ResponseEntity<String> getCriptografia() {
+		return ResponseEntity.ok("Criptografia funcionando!");
+	}
+
+}
