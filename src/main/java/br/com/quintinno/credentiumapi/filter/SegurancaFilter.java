17521be24b84459dcfe14de49f9c10cb594c5b7c
José Quintinno <josequintino@hotmail.com.br>
2025-07-09 22:36:14 -0300

    feature: Implementar Autenticação com JWT

---
+++ src/main/java/br/com/quintinno/credentiumapi/filter/SegurancaFilter.java
@@ -0,0 +1,84 @@
+package br.com.quintinno.credentiumapi.filter;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import br.com.quintinno.credentiumapi.entity.UsuarioEntity;
+import br.com.quintinno.credentiumapi.repository.UsuarioRepository;
+import br.com.quintinno.credentiumapi.service.TokenService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class SegurancaFilter extends OncePerRequestFilter {
+	
+	private static Logger logger = LoggerFactory.getLogger(SegurancaFilter.class);
+	
+	private final TokenService tokenService;
+	
+	private final UsuarioRepository usuarioRepository;
+	
+	private static final String AUTHORIZATION = "Authorization";
+	
+	private static final String BEARER = "Bearer ";
+	
+	private static final String ROLE_USER = "ROLE_USER";
+
+	public SegurancaFilter(TokenService tokenService, UsuarioRepository usuarioRepository) {
+		super();
+		this.tokenService = tokenService;
+		this.usuarioRepository = usuarioRepository;
+	}
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+		String token = this.recuperarTokenRequisicao(request);
+		String identificador = this.tokenService.validarToken(token);
+		if (identificador != null) {
+			Optional<UsuarioEntity> usuarioEntityOptional = this.usuarioRepository.findByIdentificador(identificador).stream().findFirst();
+			if (usuarioEntityOptional.isEmpty()) {
+				// FIXME: Criar Filter de TraceID para mapear cada requisicao de ponta a ponta
+			    logger.warn("CredentiumAPI [SegurancaFilter]: Usuário com identificador {} não encontrado!", identificador);
+			    filterChain.doFilter(request, response);
+			    return;
+			}
+			UsuarioEntity usuarioEntity = this.usuarioRepository.findByIdentificador(identificador).get(0);
+			if (usuarioEntity == null) {
+				logger.warn("CredentiumAPI [SegurancaFilter]: Usuário com identificador {} não encontrado!", identificador);
+				new RuntimeException("Usuário não Encontrado na Base de Dados!");
+			}
+			List<GrantedAuthority> roleList = Collections.singletonList(new SimpleGrantedAuthority(ROLE_USER));
+			UsernamePasswordAuthenticationToken authententication = new UsernamePasswordAuthenticationToken(usuarioEntity, null, roleList);
+			SecurityContextHolder.getContext().setAuthentication(authententication);
+		}
+		filterChain.doFilter(request, response);
+	}
+
+	/**
+	 * Extrai token enviado no cabecalho da requisicao
+	 * @param httpServletRequest
+	 * @return Retorna o Token sem prefixo "Bearer"
+	 */
+	private String recuperarTokenRequisicao(HttpServletRequest httpServletRequest) {
+		String header = httpServletRequest.getHeader(AUTHORIZATION);
+		if (header == null || !header.startsWith(BEARER)) {
+			logger.warn("CredentiumAPI [SegurancaFilter]: Token inválido!");
+			return null;
+		}
+		return header.replace(BEARER, "");
+	}
+
+}
---
+++ src/main/java/br/com/quintinno/credentiumapi/service/TokenService.java
@@ -0,0 +1,61 @@
+package br.com.quintinno.credentiumapi.service;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+
+import br.com.quintinno.credentiumapi.entity.UsuarioEntity;
+
+@Service
+public class TokenService {
+	
+	private static Logger logger = org.slf4j.LoggerFactory.getLogger(TokenService.class);
+	
+	@Value("${application.token.secret}")
+	private String SECRET_ALGORITHM;
+	
+	@Value("${spring.application.name}")
+	private String ORIGIN;
+	
+	@Value("${application.token.expiration}")
+	private long EXPIRATION;
+	
+	public String generateToken(UsuarioEntity usuarioEntity) {
+		try {
+			Algorithm algorithm = Algorithm.HMAC256(SECRET_ALGORITHM);
+			return JWT.create()
+					.withIssuer(ORIGIN)
+					.withSubject(usuarioEntity.getIdentificador())
+					.withExpiresAt(getTempoExpiracao())
+					.sign(algorithm);
+		} catch (Exception e) {
+			throw new RuntimeException("Erro ao gerar Token do Usuário!");
+		}
+	}
+	
+	private Date getTempoExpiracao() {
+		return Date.from(Instant.now().plus(Duration.ofHours(EXPIRATION)));
+	}
+	
+	public String validarToken(String token) {
+		try {
+			Algorithm algorithm = Algorithm.HMAC256(SECRET_ALGORITHM);
+			return JWT.require(algorithm)
+					.withIssuer(ORIGIN)
+					.build()
+					.verify(token)
+					.getSubject();
+		} catch (Exception e) {
+			logger.warn("CredentiumAPI [TokenService.class]: Falha ao validar token do usuário: {}", e.getMessage(), e);
+			return null;
+		}
+	}
+	
+}
